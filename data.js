c0f994eacff03e744c5e24d02e33df2b0bffadf2
vibora1691 <raymund.evora@gmail.com>
2026-06-01 09:26:08 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+let PEOPLE = JSON.parse(localStorage.getItem("people")) || [
+  { id: "1", name: "Deogracias Evora" },
+  { id: "2", name: "Manuel Evora" },
+  { id: "3", name: "Paulo Evora" },
+  { id: "4", name: "Carlos Evora Sr." }
+];
+
+let RELATIONS = JSON.parse(localStorage.getItem("relations")) || [
+  { parent: "1", child: "2" },
+  { parent: "1", child: "3" },
+  { parent: "3", child: "4" }
+];
+
+const MAP = {};
+function rebuildMap() {
+  MAP = {};
+  PEOPLE.forEach(p => MAP[p.id] = p);
+}
+rebuildMap();
