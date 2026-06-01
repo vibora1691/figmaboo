dab19e937d66e845c75ccf8f04707df495dae9ac
vibora1691 <raymund.evora@gmail.com>
2026-06-01 17:37:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,31 @@ const App = {
 
   render() {
 
-    rebuildMap();
+    // rebuild latest map
+    if (typeof rebuildMap === "function") {
+      rebuildMap();
+    }
 
+    // build tree
     const tree =
-      Engine.buildTree();
+      typeof Engine !== "undefined"
+        ? Engine.buildTree()
+        : {};
 
+    // build positions
     const positions =
-      Engine.buildPositions("1", tree);
+      typeof Engine !== "undefined"
+        ? Engine.buildPositions("1", tree)
+        : {};
 
-    UI.render(tree, positions);
+    // render UI
+    if (typeof UI !== "undefined") {
+      UI.render(tree, positions);
+    }
   }
 };
 
+// start app AFTER DOM is ready
 window.addEventListener("DOMContentLoaded", () => {
   App.render();
 });
